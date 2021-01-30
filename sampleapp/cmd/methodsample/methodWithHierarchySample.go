28eb6d381c1118ef03f8850f41612821db0eb66d
Esteban Collado <esteban.collado@pm.me>
2021-01-30 19:46:49 +0100

    makefiles

--- sampleapp/cmd/methodsample/methodWithHierarchySample.go
+++ sampleapp/cmd/methodsample/methodWithHierarchySample.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 )
 
+// Base struct with default implementations
 type baseListener struct {}
 
+// Default method implementations at base level
 func (bl *baseListener) buttonDownListener(origin string) {
 	fmt.Println("BaseListener buttonDownListener")
 }
@@ -14,10 +16,12 @@ func (bl *baseListener) buttonUpListener(origin string) {
 	fmt.Println("BaseListener buttonUpListener")
 }
 
+// Concrete listener extending the base one
 type concreteListener struct {
 	*baseListener
 }
 
+// The method buttonDownListener is overrider by the concrete listener
 func (cl *concreteListener) buttonDownListener(origin string) {
 	fmt.Println("ConcreteListener buttonDownListener")
 }
