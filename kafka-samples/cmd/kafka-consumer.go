68e0bf06adf2cb57f471f4f6e5c448d6e8a88db9
Esteban Collado <esteban.collado@pm.me>
2021-10-30 18:03:06 +0100

    starting kafka sample

--- kafka-samples/cmd/kafka-consumer.go
+++ kafka-samples/cmd/kafka-consumer.go
@@ -1,1 +1,22 @@
-package cmd
+package main
+
+import (
+	 "fmt"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	fmt.Println("hello")
+
+	_, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "host1:9092,host2:9092",
+		"client.id": socket.gethostname(),
+		"acks": "all"})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+}
